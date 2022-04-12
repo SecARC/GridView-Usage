fda449d7ef3c8ba808f87b703abb5827cda1e9fb
Tuğra GÜNDÜZ <trgndz1999@gmail.com>
2022-04-12 18:45:53 +0300

    GridView Usage
    GridView usage in grading system.

---
+++ week12-1/week12-1/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace week12_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grid
+            // 
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Location = new System.Drawing.Point(12, 12);
+            this.grid.Name = "grid";
+            this.grid.Size = new System.Drawing.Size(508, 364);
+            this.grid.TabIndex = 0;
+            this.grid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grid_CellContentClick);
+            this.grid.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.grid_CellEndEdit);
+            this.grid.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.grid_RowPostPaint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 383);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 407);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Find Max Grade 4";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 452);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.grid);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ week12-1/week12-1/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week12_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initGrid();
+            populateGrid();
+        }
+
+        private void initGrid()
+        {
+            grid.Columns.Clear();
+
+            DataGridViewColumn column;
+
+            column = new DataGridViewTextBoxColumn();
+            column.Name = "grade4";
+            column.HeaderText = "4 based grade";
+            column.Width = 100;
+            grid.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.Name = "grade100";
+            column.HeaderText = "100 based grade";
+            column.Width = 120;
+            grid.Columns.Add(column);
+
+            column = new DataGridViewCheckBoxColumn();
+            column.Name = "select";
+            column.HeaderText = "select";
+            column.Width = 80;
+            grid.Columns.Add(column);
+
+            column = new DataGridViewButtonColumn();
+            column.Name = "display";
+            column.HeaderText = "display";
+            column.Width = 80;
+            grid.Columns.Add(column);
+        }
+
+        private void populateGrid()
+        {
+            grid.Rows.Clear();
+
+            object[] rowItem;
+
+            rowItem = new object[] { "4,00", "100,00", true, "Display"};
+            grid.Rows.Add(rowItem);
+
+            rowItem = new object[] { "3,50", "85,00", false, "Display" };
+            grid.Rows.Add(rowItem);
+
+            rowItem = new object[] { "1,80", "68,00", true, "Display" };
+            grid.Rows.Add(rowItem);
+        }
+
+        private void grid_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            var cell = grid.Rows[e.RowIndex].Cells[e.ColumnIndex];
+
+            try
+            {
+                if (cell.Value == null)
+                {
+                    throw new ArgumentNullException("Value cannot be null!");
+                }
+
+                if(cell.OwningColumn.Name == "grade4" || cell.OwningColumn.Name == "grade100")
+                {
+                    double temp;
+                    if (double.TryParse(cell.Value.ToString(), out temp))
+                    {
+                        if (cell.OwningColumn.Name == "grade4" && temp >= 0 && temp <= 4)
+                        {
+                            // database operation can be in there
+                            label1.Text = $"grid[{e.RowIndex}][{e.ColumnIndex}] -> {temp}";
+                        }
+                        else if (cell.OwningColumn.Name == "grade100" && temp >= 0 && temp <= 100)
+                        {
+                            // database operation can be in there
+                            label1.Text = $"grid[{e.RowIndex}][{e.ColumnIndex}] -> {temp}";
+                        }
+                        else
+                        {
+                            throw new ArgumentOutOfRangeException("Value is out of range");
+                        }
+
+                    }
+                    else
+                    {
+                        throw new InvalidCastException("Value is not a valid double");
+                    }
+                }                
+            }
+            catch (Exception ex)
+            {
+                label1.Text = ex.Message;
+            }
+        }
+
+        private void grid_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            var cell = grid.Rows[e.RowIndex].Cells["grade4"];
+            var row = grid.Rows[e.RowIndex];
+
+            if (cell.Value == null)
+                return;
+
+            double temp;
+            if (double.TryParse(cell.Value.ToString(), out temp))
+            {
+                if(temp >= 2)
+                {
+                    row.DefaultCellStyle.BackColor = Color.Green;
+                }
+                else
+                {
+                    row.DefaultCellStyle.BackColor = Color.Red;
+                }
+            }
+        }
+
+        private void grid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var column = grid.Columns[e.ColumnIndex];
+            var cell = grid.Rows[e.RowIndex].Cells["grade4"];
+
+            if (cell.Value == null)
+                return;
+
+            if (column is DataGridViewButtonColumn && column.Name == "display")
+            {
+                double temp = double.Parse(cell.Value.ToString());
+                
+                    string msg = string.Format("You selected the grid[{0}][{1}]: {2:#0.00}", e.RowIndex, e.ColumnIndex, temp);
+                MessageBox.Show(msg);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double max = 0;
+
+            foreach (DataGridViewRow row in grid.Rows)
+            {
+                if (row.Cells["grade4"].Value == null)
+                    continue;
+
+                double temp = double.Parse(row.Cells["grade4"].Value.ToString());
+                if (temp > max)
+                    max = temp;
+            }
+
+            MessageBox.Show(string.Format("Max value of the grades 4 is : {0:0.00}", max));
+        }
+    }
+}
